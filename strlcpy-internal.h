9035ff9f802cae90997a730056bdf58a9234ae63
tuuuuuu <83738345+MasteringT@users.noreply.github.com>
2021-09-06 11:41:28 +0800

    Update strlcpy-internal.h

--- strlcpy-internal.h
+++ strlcpy-internal.h
@@ -14,7 +14,7 @@ extern "C" { // 扩展，可使用C中函数
 #ifndef HAVE_STRLCPY
 #include <string.h>
 size_t _event_strlcpy(char *dst, const char *src, size_t siz);
-#define strlcpy _event_strlcpy  用自定义的_event_strlcpy替换C语言中自带的strlcpy
+#define strlcpy _event_strlcpy  // 用自定义的_event_strlcpy替换C语言中自带的strlcpy
 #endif
 
 #ifdef __cplusplus
