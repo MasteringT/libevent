0e4e1265907cbcc33d2d4aea039705aaf36337d9
tuuuuuu <83738345+MasteringT@users.noreply.github.com>
2021-09-06 11:41:15 +0800

    Update strlcpy-internal.h

--- strlcpy-internal.h
+++ strlcpy-internal.h
@@ -1,7 +1,9 @@
 #ifndef _STRLCPY_INTERNAL_H_
 #define _STRLCPY_INTERNAL_H_
 
-#ifdef __cplusplus
+// 条件编译：1. 只有C++中能用extern "C"，否则会报错； 2. 避免项目中因为多处include出现冗余定义造成错误。
+// 参考https://www.cnblogs.com/skynet/archive/2010/07/10/1774964.html
+#ifdef __cplusplus  
 extern "C" { // 扩展，可使用C中函数
 #endif
 
